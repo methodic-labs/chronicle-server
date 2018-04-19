a380c87e891df9cc74a78b6cdd5f5ddaa54bc8e5
Marie Reed <marie@openlattice.com>
2018-04-19 10:02:02 -0700

    Removed non chronicle endpoints that belond in dataApi.

--- src/main/java/com/openlattice/chronicle/controllers/ChronicleController.java
+++ src/main/java/com/openlattice/chronicle/controllers/ChronicleController.java
@@ -2,19 +2,15 @@ package com.openlattice.chronicle.controllers;
 
 import com.openlattice.chronicle.services.ChronicleService;
 import com.openlattice.chronicle.ChronicleApi;
-import com.openlattice.chronicle.constants.CustomMediaType;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.inject.Inject;
 import java.util.UUID;
-import java.util.Set;
 
-// TODO: Do I need the Override
 @RestController
 @RequestMapping( ChronicleApi.CONTROLLER )
 public class ChronicleController implements ChronicleApi {
@@ -42,152 +38,5 @@ public class ChronicleController implements ChronicleApi {
                               @PathVariable( PARTICIPANT_ID ) UUID participantId,
                               @PathVariable( DEVICE_ID ) String deviceId) {
         ChronicleService.enrollDevice( studyId, participantId, deviceId );
-    }
-
-    @Override
-    @RequestMapping(
-            path = STUDY_PATH,
-            method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE )
-    public void createStudy( @RequestBody Study study) {
-        ChronicleService.createStudy( study );
-
-    }
-
-    @Override
-    @RequestMapping(
-            path = STUDY_ID_PATH,
-            method = RequestMethod.DELETE,
-            consumes = MediaType.APPLICATION_JSON_VALUE )
-    public void deleteStudy( @PathVariable( STUDY_ID ) UUID studyId) {
-        ChronicleService.deleteStudy( studyId );
-    }
-
-    @Override
-    @RequestMapping(
-            path = STUDY_PATH,
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE )
-    public Iterable<Study> getAllStudies() {
-        ChronicleService.getAllStudies();
-    }
-
-    @Override
-    @RequestMapping(
-            path = STUDY_PATH + STUDY_ID_PATH,
-            method = RequestMethod.GET,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE )
-    public Study getStudyById( @PathVariable( STUDY_ID ) UUID studyId) {
-        ChronicleService.getStudyById( studyId );
-    }
-
-    @Override
-    @RequestMapping(
-            path = PARTICIPANT_PATH,
-            method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE )
-    public Study createParticipant( @RequestBody Person participant) {
-        ChronicleService.createParticipant( participant );
-    }
-
-    @Override
-    @RequestMapping(
-            path = PARTICIPANT_ID_PATH,
-            method = RequestMethod.DELETE,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE )
-    public Study deleteParticipant( @PathVariable( PARTICIPANT_ID ) UUID participantId) {
-        ChronicleService.deleteParticipant( participantId );
-    }
-
-    @Override
-    @RequestMapping(
-            path = PARTICIPANT_PATH,
-            method = RequestMethod.GET,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE )
-    public Iterable<Person> getAllParticipants() {
-        ChronicleService.getAllParticipants();
-    }
-
-    @Override
-    @RequestMapping(
-            path = STUDY_ID_PATH + PARTICIPANT_PATH,
-            method = RequestMethod.GET,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE )
-    public Iterable<Person> getParticipantsFromStudy( @PathVariable( STUDY_ID ) UUID studyId) {
-        ChronicleService.getParticipantsFromStudy( studyId );
-    }
-
-    @Override
-    @RequestMapping(
-            path = PARTICIPANT_ID_PATH,
-            method = RequestMethod.GET,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE )
-    public Person getParticipantById( @PathVariable( PARTICIPANT_ID ) UUID participantId) {
-        ChronicleService.getParticipantById( participantId );
-    }
-
-    @Override
-    @RequestMapping(
-            path = UPDATE_PATH + PARTICIPANT_ID_PATH,
-            method = RequestMethod.PATCH,
-            consumes = MediaType.APPLICATION_JSON_VALUE )
-    public void updateParticipantMetadata( @PathVariable( PARTICIPANT_ID ) UUID participantId,
-                                           @RequestBody MetadataUpdate metadataUpdate) {
-        ChronicleService.updateParticipantMetadata( participantId, metadataUpdate );
-    }
-
-    @Override
-    @RequestMapping(
-            path = UPDATE_PATH + STUDY_ID_PATH,
-            method = RequestMethod.PATCH,
-            consumes = MediaType.APPLICATION_JSON_VALUE )
-    public void updateStudyMetadata( @PathVariable( STUDY_ID ) UUID studyId,
-                                     @RequestBody MetadataUpdate metadataUpdate){
-        ChronicleService.updateStudyMetadata( studyId, metadataUpdate );
-    }
-
-    @Override
-    @RequestMapping(
-            path = INSTALL_PATH + STUDY_ID_PATH + PARTICIPANT_ID_PATH,
-            method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE )
-    public void addParticipantToStudy( @PathVariable( STUDY_ID )UUID studyId,
-                                       @PathVariable( PARTICIPANT_ID ) UUID participantId) {
-        ChronicleService.addParticipantToStudy( studyId, participantId );
-    }
-
-    @Override
-    @RequestMapping(
-            path = INSTALL_PATH + STUDY_ID_PATH + PARTICIPANT_ID_PATH,
-            method = RequestMethod.DELETE,
-            consumes = MediaType.APPLICATION_JSON_VALUE )
-    public void removeParticipantFromStudy( @PathVariable( STUDY_ID )UUID studyId,
-                                            @PathVariable( PARTICIPANT_ID ) UUID participantId) {
-        ChronicleService.removeParticipantFromStudy( studyId, participantId );
-    }
-
-    @Override
-    @RequestMapping(
-            path = INSTALL_PATH + STUDY_ID_PATH + BULK_PATH,
-            method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE )
-    public void addParticipantsToStudy( @PathVariable( STUDY_ID )UUID studyId,
-                                       @PathVariable( PARTICIPANT_ID ) UUID participantId) {
-        ChronicleService.addParticipantsToStudy( studyId, participantId );
-    }
-    @Override
-    @RequestMapping(
-            path = INSTALL_PATH + STUDY_ID_PATH + BULK_PATH,
-            method = RequestMethod.DELETE,
-            consumes = MediaType.APPLICATION_JSON_VALUE )
-    public void removeParticipantsFromStudy( @PathVariable( STUDY_ID )UUID studyId,
-                                              @PathVariable( PARTICIPANT_ID ) UUID participantId) {
-        ChronicleService.removeParticipantsFromStudy( studyId, participantId );
     }
 }
--- src/main/java/com/openlattice/chronicle/services/ChronicleService.java
+++ src/main/java/com/openlattice/chronicle/services/ChronicleService.java
@@ -7,111 +7,20 @@ import com.openlattice.authorization.HazelcastAclKeyReservationService;
 
 import javax.inject.Inject;
 import java.util.UUID;
+//  TODO: Need the information for the data parsing to do logData()
 
 public class ChronicleService {
-    private final IMap<UUID, Study>                    studies;
-    private final IMap<UUID, Person>                   participants;
-
-    private final HazelcastAclKeyReservationService    reservations;
 
     @Inject
     private EventBus eventBus;
 
-    public ChronicleService(
-            HazelcastInstance hazelcast,
-            HazelcastAclKeyReservationService reservations) {
-        this.studies = hazelcast.getMap( HazelcastMap.STUDIES.name() );
-        this.participants = hazelcast.getMap( HazelcastMap.PARTICIPANTS.name() );
-        this.reservations = reservations;
-    }
+    public ChronicleService() { }
 
     public void logData( studyId, participantId ) {
-//        TODO
+//        TODO: Parse the data and integrate with datamodel
     }
 
     public void enrollDevice( studyId, participantId, deviceId ) {
-//        TODO
-    }
-
-    public UUID createStudy( study ) {
-        reservations.reserveIdAndValidateType( study, study::getName );
-        studies.putIfAbsent( study.getId(), study );
-//        TODO: edm.events.StudyCreatedEvent
-        eventBus.post( new StudyCreatedEvent( study ) );
-        return study.getId();
-
-    }
-
-    public void deleteStudy( studyId ) {
-        studies.delete( studyId );
-//        Don't release the studyId because it might conflict with external records
-//        TODO: edm.events.StudyDeletedEvent
-        eventBus.post( new StudyDeletedEvent( studyId ) );
-
-    }
-
-    public Iterable<Study> getAllStudies() {
-        return studies.values();
-    }
-
-    public Study getStudyById( studyId ) {
-        return studies.get( studyId );
-    }
-
-    public UUID createParticipant( participant ) {
-        reservations.reserveIdAndValidateType( participant, participant::getName );
-        studies.putIfAbsent( participant.getId(), participant );
-//        TODO: edm.events.PersonCreatedEvent
-        eventBus.post( new PersonCreatedEvent( participant ) );
-        return participant.getId();
-    }
-
-    public void deleteParticipant( participantId ) {
-        participants.delete( participantId );
-//        Don't release the participantId because it might conflict with external records
-//        TODO: edm.events.PersonDeletedEvent
-        eventBus.post( new PersonDeletedEvent( studyId ) );
-    }
-
-    public Iterable<Person> getAllParticipants() {
-//        Here I only want people who are created from the chronicle web, not all people.
-//        We will be displaying them by ID not name, as they will not have names
-//        TODO
-    }
-
-    public Iterable<Person> getParticipantsFromStudy( studyId ) {
-//        TODO: This may be a front end task. Or can I do studies.getAll(participants)?
-    }
-
-    public Person getParticipantById( participantId ) {
-        return participants.get( participantId );
-    }
-
-    public void updateParticipantMetadata( participantId, metadataUpdate ) {
-//        TODO: UpdatePersonMetadataProcessor
-        participants.executeOnKey( participantId, new UpdatePersonMetadataProcessor( metadataUpdate ) );
-        eventBus.post( new PersonCreatedEvent( participants.get( participantId ) ) );
-    }
-
-    public void updateStudyMetadata( studyId, metadataUpdate ) {
-//        TODO: UpdateStudyMetadataProcessor
-        studies.executeOnKey( studyId, new UpdateStudyMetadataProcessor( metadataUpdate ) );
-        eventBus.post( new StudyCreatedEvent( studies.get( studyId ) ) );
-    }
-
-    public void addParticipantToStudy( studyId, participantId ) {
-//        TODO
-    }
-
-    public void removeParticipantFromStudy( studyId, participantId ) {
-//        TODO
-    }
-
-    public void addParticipantsToStudy( studyId, participantId ) {
-//        TODO
-    }
-
-    public void removeParticipantsFromStudy( studyId, participantId ) {
 //        TODO
     }
 
