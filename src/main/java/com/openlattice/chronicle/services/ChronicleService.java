f7babc9c3200aa320dca52c868de7141fd89a060
Marie Reed <marie@openlattice.com>
2018-04-20 09:30:59 -0700

    Added comments

--- src/main/java/com/openlattice/chronicle/services/ChronicleService.java
+++ src/main/java/com/openlattice/chronicle/services/ChronicleService.java
@@ -4,6 +4,7 @@ import com.openlattice.data.DataApi;
 import com.google.common.collect.SetMultimap;
 import com.google.common.eventbus.EventBus;
 import com.openlattice.data.requests.BulkDataCreation;
+import com.openlattice.data.requests.Association;
 
 import javax.inject.Inject;
 import java.util.UUID;
@@ -17,27 +18,27 @@ public class ChronicleService {
     public ChronicleService() { }
 
     public void logData(UUID studyId, UUID participantId, String deviceId, UUID entitySetId, SetMultimap<UUID, Object> data) {
-//     create the syncTicket
+//     create the syncTickets
 //          create a fake syncId UUID
 //              How is this done? can I use xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx
 //          DataApi.acquireSyncTicket( entitySetId, fakeSyncId )
 
 //     create the associations
-//          Association( Key, srcKey, dstKey, data )
-//          EntityKey( entitySetId (uuid), syncId (uuid), entityId (string) )
-//          Key = (entitySetId, fakeSyncId, ? )
-//          srcKey = (entitySetIdOfSrc, fakeSyncId, ? ) Do I use the same syncId?
-//          dstKey = (entitySetIdOfDst, fakeSyncId, ? ) Do I use the same syncId?
+//        var association = new Association( Key, srcKey, dstKey, data )
+//              create one for each association and then combine into Set<associations>
+//              EntityKey = ( entitySetId (uuid), syncId (uuid), entityId (string) )
+//              Entity = ( EntityKey, data )
+//          Key = ( entitySetId, fakeSyncId, ? )
+//          srcKey = ( entitySetIdOfSrc, fakeSyncId, ? ) Do I use the same syncId?
+//          dstKey = ( entitySetIdOfDst, fakeSyncId, ? ) Do I use the same syncId?
 
 //     create the BulkData
-//          var bulkData = new BulkDataCreation(syncTicket, data, associations)
+//          var bulkData = new BulkDataCreation( Set<syncTickets>, Set<entities>, Set<associations> )
 //     DataApi.createEntityAndAssociationData( BulkDataCreation bulkData );
     }
 
     public void enrollDevice( UUID studyId, UUID participantId, String deviceId ) {
-//     verify the participant is enrolled in study ( verifyParticipant() --> true )
-//     check to make sure the device does not already exist ( aka verifyDevice() --> false )
-//     If false, then add the device and associate to the participant and to the study
+//     add the device and associate to the participant and to the study
     }
 
 }
