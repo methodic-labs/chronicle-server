c4f4459a86e883d7f8b3bae481604c53a3404f8e
Marie Reed <marie@openlattice.com>
2018-04-20 15:06:56 -0700

    Set out format to fill in controller once data Model is ready.

--- src/main/java/com/openlattice/chronicle/services/ChronicleService.java
+++ src/main/java/com/openlattice/chronicle/services/ChronicleService.java
@@ -25,32 +25,47 @@ public class ChronicleService {
             UUID entitySetId,
             SetMultimap<UUID, Object> data ) {
         //  TODO: Need the data model to exist before I can do anything here.
-        //  create the syncTickets
-        //  create a fake syncId UUID
-        //  How is this done? can I use xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx
-        //  DataApi.acquireSyncTicket( entitySetId, fakeSyncId )
 
-        //  create the associations
-        //  var association = new Association( Key, srcKey, dstKey, data )
-        //  create one for each association and then combine into Set<associations>
-        //  EntityKey = ( entitySetId (uuid), syncId (uuid), entityId (string) )
-        //  Entity = ( EntityKey, data )
-        //  Key = ( entitySetId, fakeSyncId, ? )
-        //  srcKey = ( entitySetIdOfSrc, fakeSyncId, ? ) Do I use the same syncId?
-        //  dstKey = ( entitySetIdOfDst, fakeSyncId, ? ) Do I use the same syncId?
-
-        //  create the BulkData
+        //  TEMPLATE FOR BULK DATA CREATION
         //  var bulkData = new BulkDataCreation( Set<syncTickets>, Set<entities>, Set<associations> )
+        //      Set<syncTickets> = empty set
+
+        //      Set<entities> = ( entityKey, details )
+        //          FOR EACH ENTITY ( for each: individual logs )
+        //          entity = ( entityKey, details )
+        //              entityKey = ( entitySetId (uuid), syncId (uuid), entityId (string) )
+        //                  entitySetId = entitySetId --> PathVariable
+        //                  syncId = SyncApi.getCurrentSyncId(entitySetId)
+        //                  entityId = TODO this will be the logData entitySet
+        //              data = data --> PathVariable
+
+        //      Set<associations> = ( key, src, dst, details )
+        //          FOR EACH ASSOCIATION ( for each: log --> Person, Study, Device )
+        //          key = ( entitySetId (uuid), syncId (uuid), entityId (string) ) TODO this will be the association entitySet
+        //              entitySetId = entitySetId --> PathVariable
+        //              syncId = SyncApi.getCurrentSyncId(entitySetId)
+        //              entityId = TODO
+        //          src = ( entitySetId (uuid), syncId (uuid), entityId (string) )
+        //              entitySetId = TODO
+        //              syncId = SyncApi.getCurrentSyncId(TODO)
+        //              entityId = TODO        //          dst =
+        //          dst = ( entitySetId (uuid), syncId (uuid), entityId (string) )
+        //              entitySetId = TODO
+        //              syncId = SyncApi.getCurrentSyncId(TODO)
+        //              entityId = TODO
+        //          details = TODO put here what I want in the association set
+
+        //
+
         //  DataApi.createEntityAndAssociationData( BulkDataCreation bulkData );
     }
 
     public void enrollDevice( UUID studyId, UUID participantId, String deviceId ) {
-        //  TODO: Need the data model to exist before I can do anything here.
         //  previous logic already verified the participant and that the device is not already connected.
         //  add the device and associate to the participant and to the study
         //  this will be two associations device --> person, device --> study
         //  aka write a line to these association tables, createAssociationData() does not exist in lattice-js yet.
-        //  DataApi.createEntityAndAssociationData()
+        //  DataApi.createEntityAndAssociationData() see example above for template
     }
 
 }
