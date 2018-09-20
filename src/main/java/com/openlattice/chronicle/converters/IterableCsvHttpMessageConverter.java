ab13f6fabd67e6e91975971b5ecba126906ae5cf
Hristo Oskov <hristo.oskov@gmail.com>
2018-09-20 12:12:28 -0700

    participant data download implementation

--- src/main/java/com/openlattice/chronicle/converters/IterableCsvHttpMessageConverter.java
+++ src/main/java/com/openlattice/chronicle/converters/IterableCsvHttpMessageConverter.java
@@ -15,28 +15,33 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * You can contact the owner of the copyright at support@openlattice.com
- *
  */
 
 package com.openlattice.chronicle.converters;
 
-import java.io.IOException;
-import java.lang.reflect.Type;
-
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.csv.CsvGenerator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema.Builder;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema.ColumnType;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
+import com.openlattice.chronicle.constants.CustomMediaType;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.converter.AbstractGenericHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema.Builder;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema.ColumnType;
-import com.fasterxml.jackson.datatype.guava.GuavaModule;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
-import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
-import com.google.common.collect.Multimap;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Set;
 
 public class IterableCsvHttpMessageConverter
         extends AbstractGenericHttpMessageConverter<Iterable<Multimap<String, ?>>> {
@@ -44,30 +49,26 @@ public class IterableCsvHttpMessageConverter
     private final CsvMapper csvMapper = new CsvMapper();
 
     public IterableCsvHttpMessageConverter() {
+        super( CustomMediaType.TEXT_CSV );
         csvMapper.registerModule( new AfterburnerModule() );
         csvMapper.registerModule( new GuavaModule() );
         csvMapper.registerModule( new JodaModule() );
+        csvMapper.registerModule( new JavaTimeModule() );
+        csvMapper.registerModule( new Jdk8Module() );
+        csvMapper.configure( SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false );
+        csvMapper.configure( CsvGenerator.Feature.STRICT_CHECK_FOR_QUOTING, true );
     }
 
     @Override
     public Iterable<Multimap<String, ?>> read( Type type, Class<?> contextClass, HttpInputMessage inputMessage )
-            throws IOException, HttpMessageNotReadableException {
+            throws HttpMessageNotReadableException {
         throw new UnsupportedOperationException( "CSV is not a supported input format" );
     }
 
     @Override
     protected void writeInternal( Iterable<Multimap<String, ?>> t, Type type, HttpOutputMessage outputMessage )
             throws IOException, HttpMessageNotWritableException {
-        CsvSchema schema = null;
-
-        for ( Multimap<String, ?> obj : t ) {
-            if ( schema == null ) {
-                schema = fromMultimap( obj );
-            }
-            // TODO: Flatten or drop multiple output values into obj.
-            csvMapper.writer( schema ).writeValue( outputMessage.getBody(), obj.asMap() );
-        }
-
+        csvMapper.writer( getSchema( t ) ).writeValues( outputMessage.getBody() ).writeAll( t );
     }
 
     @Override
@@ -78,17 +79,18 @@ public class IterableCsvHttpMessageConverter
     @Override
     protected Iterable<Multimap<String, ?>> readInternal(
             Class<? extends Iterable<Multimap<String, ?>>> clazz,
-            HttpInputMessage inputMessage )
-            throws IOException, HttpMessageNotReadableException {
+            HttpInputMessage inputMessage ) throws HttpMessageNotReadableException {
         throw new UnsupportedOperationException( "CSV is not a supported input format" );
     }
 
-    public CsvSchema fromMultimap( Multimap<String, ?> m ) {
+    private CsvSchema getSchema( Iterable<Multimap<String, ?>> maps ) {
+        Set<String> columns = Sets.newLinkedHashSet();
+        maps.forEach( map -> columns.addAll( map.keySet() ) );
         Builder schemaBuilder = CsvSchema.builder();
-
-        m.keySet().stream().forEach( type -> schemaBuilder.addColumn( type, ColumnType.ARRAY ) );
-        schemaBuilder.setUseHeader( true );
-        return schemaBuilder.build();
+        columns.forEach( column -> schemaBuilder.addColumn( column, ColumnType.ARRAY ) );
+        return schemaBuilder
+                .setUseHeader( true )
+                .build();
     }
 
 }
