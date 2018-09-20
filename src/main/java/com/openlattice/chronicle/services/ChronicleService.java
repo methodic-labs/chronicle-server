58bc6556372c7797040a178a146276fc3e08649f
Hristo Oskov <hristo.oskov@gmail.com>
2018-09-20 12:18:56 -0700

    participant data download implementation

--- src/main/java/com/openlattice/chronicle/services/ChronicleService.java
+++ src/main/java/com/openlattice/chronicle/services/ChronicleService.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2018. OpenLattice, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact the owner of the copyright at support@openlattice.com
+ */
+
 package com.openlattice.chronicle.services;
 
 import com.google.common.base.Optional;
@@ -29,4 +48,6 @@ public interface ChronicleService {
     boolean isKnownParticipant( UUID studyId, String participantId );
 
     Map<String, UUID> getPropertyTypeIds( Set<String> propertyTypeFqns );
+
+    Iterable<SetMultimap<String, Object>> getAllParticipantData( UUID studyId, UUID participantEntityId );
 }
