4a52697941860a4b7192ff20a37466dccae711ae
jokedurnez <joke.durnez@gmail.com>
2020-06-09 14:06:16 -0700

    unused import

--- src/main/java/com/openlattice/chronicle/services/ChronicleServiceImpl.java
+++ src/main/java/com/openlattice/chronicle/services/ChronicleServiceImpl.java
@@ -61,8 +61,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import com.openlattice.chronicle.data.DeleteType;
-
 import javax.annotation.Nonnull;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
