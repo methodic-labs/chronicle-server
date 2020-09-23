79c18371f49d82fca4b6eb73fcfac581aeef19cb
Alfonce Nzioka <alfonce.starfish@gmail.com>
2020-09-23 07:07:30 -0700

    remove unused

--- src/main/java/com/openlattice/chronicle/constants/EdmConstants.java
+++ src/main/java/com/openlattice/chronicle/constants/EdmConstants.java
@@ -21,7 +21,6 @@ public class EdmConstants {
     public static final FullQualifiedName COMPLETED_DATE_TIME_FQN = new FullQualifiedName( "date.completeddatetime" );
     public static final FullQualifiedName DATE_LOGGED_FQN         = new FullQualifiedName( "ol.datelogged" );
     public static final FullQualifiedName DATE_TIME_FQN           = new FullQualifiedName( "ol.datetime" );
-    public static final FullQualifiedName DURATION_FQN            = new FullQualifiedName( "general.Duration" );
     public static final FullQualifiedName END_DATE_TIME_FQN       = new FullQualifiedName( "ol.datetimeend" );
     public static final FullQualifiedName FULL_NAME_FQN           = new FullQualifiedName( "general.fullname" );
     public static final FullQualifiedName MODEL_FQN               = new FullQualifiedName( "vehicle.model" );
@@ -34,14 +33,9 @@ public class EdmConstants {
     public static final FullQualifiedName STRING_ID_FQN           = new FullQualifiedName( "general.stringid" );
     public static final FullQualifiedName TIMEZONE_FQN            = new FullQualifiedName( "ol.timezone" );
     public static final FullQualifiedName TITLE_FQN               = new FullQualifiedName( "ol.title" );
-    public static final FullQualifiedName USER_FQN                = new FullQualifiedName( "ol.user" );
     public static final FullQualifiedName VALUES_FQN              = new FullQualifiedName( "ol.values" );
     public static final FullQualifiedName VERSION_FQN             = new FullQualifiedName( "ol.version" );
 
-    // entity types
-    public static final FullQualifiedName PERSON_FQN = new FullQualifiedName( "general.person" );
-
     // CAFE ORG: to maintain backward compatibility, all studies before apps v2 will be assumed to belong to CAFE ORG
     public static final UUID CAFE_ORG_ID = UUID.fromString( "7349c446-2acc-4d14-b2a9-a13be39cff93" );
-
 }
