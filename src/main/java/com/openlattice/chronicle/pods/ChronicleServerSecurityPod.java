13cc1ad6035a33ebc75ec3b39319f6e74182e3d5
Alfonce Nzioka <alfonce.starfish@gmail.com>
2021-07-19 13:15:39 -0700

    use CsrfFilter

--- src/main/java/com/openlattice/chronicle/pods/ChronicleServerSecurityPod.java
+++ src/main/java/com/openlattice/chronicle/pods/ChronicleServerSecurityPod.java
@@ -27,7 +27,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
+import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.nio.charset.StandardCharsets;
@@ -46,11 +46,11 @@ public class ChronicleServerSecurityPod extends Auth0SecurityPod {
         http.authorizeRequests()
                 .antMatchers( HttpMethod.OPTIONS ).permitAll()
                 .antMatchers( HttpMethod.POST, "/chronicle/data/study/participant" ).permitAll()
-                .antMatchers ( "/chronicle/data/authenticated/**" ).authenticated();
+                .antMatchers( "/chronicle/data/authenticated/**" ).authenticated();
 
         CharacterEncodingFilter filter = new CharacterEncodingFilter();
         filter.setEncoding( StandardCharsets.UTF_8.toString() );
         filter.setForceEncoding( true );
-        http.addFilterBefore( filter, WebAsyncManagerIntegrationFilter.class );
+        http.addFilterBefore( filter, CsrfFilter.class );
     }
 }
