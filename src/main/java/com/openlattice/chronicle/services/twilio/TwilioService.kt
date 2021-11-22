ee83775bc6aedb6bd9aa5e2fcb6361eb94088a00
Todd Bergman <todd@openlattice.com>
2021-11-22 09:59:17 -0600

    pass orgid and set status callback

--- src/main/java/com/openlattice/chronicle/services/twilio/TwilioService.kt
+++ src/main/java/com/openlattice/chronicle/services/twilio/TwilioService.kt
@@ -1,5 +1,6 @@
 package com.openlattice.chronicle.services.twilio
 
+import com.openlattice.chronicle.api.ChronicleApi.*
 import com.openlattice.chronicle.configuration.TwilioConfiguration
 import com.openlattice.chronicle.data.MessageDetails
 import com.openlattice.chronicle.data.MessageOutcome
@@ -7,17 +8,19 @@ import com.twilio.Twilio
 import com.twilio.exception.ApiException
 import com.twilio.rest.api.v2010.account.Message
 import com.twilio.type.PhoneNumber
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.net.URI
 import java.time.OffsetDateTime
+import java.util.*
 
 /**
  * @author toddbergman &lt;todd@openlattice.com&gt;
  */
-class TwilioService(configuration: TwilioConfiguration) :
+open class TwilioService(configuration: TwilioConfiguration) :
     TwilioManager {
     companion object {
-        protected val logger = LoggerFactory.getLogger(TwilioService::class.java)
+        protected val logger: Logger = LoggerFactory.getLogger(TwilioService::class.java)
     }
 
     init {
@@ -26,13 +29,21 @@ class TwilioService(configuration: TwilioConfiguration) :
 
     private val fromPhoneNumber: PhoneNumber = PhoneNumber(configuration.fromPhone)
 
-    private fun sendMessage(messageDetails: MessageDetails) :MessageOutcome {
+    private fun sendMessage(organizationId: UUID, messageDetails: MessageDetails) :MessageOutcome {
         val messageText = "Chronicle device enrollment:  Please download app from your app store and click on ${messageDetails.url} to enroll your device."
         try {
             val message = Message
                 .creator(PhoneNumber(messageDetails.phoneNumber), fromPhoneNumber, messageText)
-                .setStatusCallback(URI.create("https://api.openlattice.com/bifrost/messages/status"))
+                .setStatusCallback(URI.create(
+                    "https://api.openlattice.com$BASE/$organizationId$MESSAGE_PATH$STATUS_PATH"))
                 .create()
+            logger.info(
+                """
+                    message sent to participant ${messageDetails.participantId}
+                    for study ${messageDetails.studyId}
+                    in organization $messageDetails
+                """.trimIndent()
+            )
             return MessageOutcome(
                 messageDetails.messageType,
                 OffsetDateTime.now(),
@@ -60,7 +71,7 @@ class TwilioService(configuration: TwilioConfiguration) :
         }
     }
 
-    override fun sendMessages(messageDetailsList: List<MessageDetails>): List<MessageOutcome> {
-        return messageDetailsList.map { messageDetails -> sendMessage(messageDetails) }
+    override fun sendMessages(organizationId: UUID, messageDetailsList: List<MessageDetails>): List<MessageOutcome> {
+        return messageDetailsList.map { messageDetails -> sendMessage(organizationId, messageDetails) }
     }
 }
