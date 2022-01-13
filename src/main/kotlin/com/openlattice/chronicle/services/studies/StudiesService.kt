a74586dd3cc034b820d276999ca8ed2bfc0a1cc3
Solomon Tang <solomoncztang@gmail.com>
2022-01-12 20:27:58 -0800

    sql to insert study

--- src/main/kotlin/com/openlattice/chronicle/services/studies/StudiesService.kt
+++ src/main/kotlin/com/openlattice/chronicle/services/studies/StudiesService.kt
@@ -3,7 +3,9 @@ package com.openlattice.chronicle.services.studies
 import com.openlattice.chronicle.storage.StorageResolver
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.openlattice.chronicle.study.Study
+import com.openlattice.chronicle.storage.ChroniclePostgresTables.Companion.STUDIES
 import org.slf4j.LoggerFactory
+import java.time.LocalDate
 import java.util.UUID
 
 /**
@@ -23,7 +25,39 @@ class StudiesService(
         study: Study,
     ): UUID {
         val studyId = UUID.randomUUID()
-
+        try {
+            val (flavor, hds) = storageResolver.resolve(studyId)
+            hds.connection.createStatement().execute(
+                insertStudySql(
+                    organizationId,
+                    studyId,
+                    study
+                )
+            )
+        } catch (e: Exception) {
+            logger.error("hds error: $e")
+        }
         return studyId
+    }
+
+    private fun insertStudySql(
+        organizationId: UUID,
+        studyId: UUID,
+        study: Study,
+    ): String {
+
+        return """
+            INSERT INTO ${STUDIES.name} VALUES (
+            '${organizationId},
+            '${studyId}'
+            '${study.title}',
+            '${study.description}',
+            '${LocalDate.now()}',
+            '${LocalDate.now()}',
+            '${study.startedAt}',
+            '${study.endedAt}',
+            '${study.group},
+            '${study.version}')
+        """.trimIndent()
     }
 }
