9fe4c421b6f48960b4acb9c60ed747aef58eff76
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-01-20 12:44:50 -0800

    annotations

--- src/main/kotlin/com/openlattice/chronicle/controllers/v3/AppUsageSurveyController.kt
+++ src/main/kotlin/com/openlattice/chronicle/controllers/v3/AppUsageSurveyController.kt
@@ -1,10 +1,24 @@
 package com.openlattice.chronicle.controllers.v3
 
+import com.codahale.metrics.annotation.Timed
 import com.openlattice.chronicle.services.surveys.SurveysService
 import com.openlattice.chronicle.survey.AppUsage
 import com.openlattice.chronicle.survey.AppUsageSurveyApi
 import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.CONTROLLER
+import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.DATE
+import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.ORGANIZATION_ID
+import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.ORGANIZATION_ID_PATH
+import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.PARTICIPANT_ID
+import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.PARTICIPANT_ID_PATH
+import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.STUDY_ID
+import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.STUDY_ID_PATH
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 import javax.inject.Inject
@@ -15,16 +29,36 @@ import javax.inject.Inject
 
 @RestController
 @RequestMapping(CONTROLLER)
-class AppUsageSurveyController: AppUsageSurveyApi {
+class AppUsageSurveyController : AppUsageSurveyApi {
 
     @Inject
     private lateinit var surveysService: SurveysService
 
-    override fun getAppUsageData(organizationId: UUID, studyId: UUID, participantId: String, date: String): List<AppUsage> {
+    @Timed
+    @GetMapping(
+            path = [ORGANIZATION_ID_PATH + STUDY_ID_PATH + PARTICIPANT_ID_PATH],
+            produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    override fun getAppUsageData(
+            @PathVariable(ORGANIZATION_ID) organizationId: UUID,
+            @PathVariable(STUDY_ID) studyId: UUID,
+            @PathVariable(PARTICIPANT_ID) participantId: String,
+            @RequestParam(value = DATE) date: String
+    ): List<AppUsage> {
         return surveysService.getAppUsageData(organizationId, studyId, participantId, date)
     }
 
-    override fun submitAppUsageSurvey(organizationId: UUID, studyId: UUID, participantId: String, surveyResponses: Map<UUID, Set<String>>) {
+    @Timed
+    @PostMapping(
+            path = [ORGANIZATION_ID_PATH + STUDY_ID_PATH + PARTICIPANT_ID_PATH],
+            consumes = [MediaType.APPLICATION_JSON_VALUE],
+    )
+    override fun submitAppUsageSurvey(
+            @PathVariable(ORGANIZATION_ID) organizationId: UUID,
+            @PathVariable(STUDY_ID) studyId: UUID,
+            @PathVariable(PARTICIPANT_ID) participantId: String,
+            @RequestBody surveyResponses: Map<UUID, Set<String>>
+    ) {
         surveysService.submitAppUsageSurvey(organizationId, studyId, participantId, surveyResponses)
     }
 }
