52358a67fba6b86e038cfb4cee22a20dfa42f493
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-01-20 02:38:36 -0800

    rename col

--- src/main/kotlin/com/openlattice/chronicle/storage/PostgresColumns.kt
+++ src/main/kotlin/com/openlattice/chronicle/storage/PostgresColumns.kt
@@ -68,7 +68,7 @@ class PostgresColumns {
         val LON = PostgresColumnDefinition("lon", PostgresDatatype.DOUBLE)
 
         // app usage survey specific columns
-        val APP_LABEL = PostgresColumnDefinition("app_LABEL", PostgresDatatype.TEXT)
+        val APP_LABEL = PostgresColumnDefinition("app_label", PostgresDatatype.TEXT)
         val APP_PACKAGE_NAME = PostgresColumnDefinition("app_package_name", PostgresDatatype.TEXT).notNull()
         val APP_USAGE_ID = PostgresColumnDefinition("id", PostgresDatatype.TEXT_UUID).notNull()
         val APP_USAGE_USERS = PostgresColumnDefinition("app_users", PostgresDatatype.TEXT_ARRAY)
