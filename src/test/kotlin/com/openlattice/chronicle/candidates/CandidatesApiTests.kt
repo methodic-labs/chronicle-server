cb3e46cdcaa20526d0e3febfde5062422202665c
Hristo Oskov <hristo@openlattice.com>
2022-01-27 13:51:11 -0800

    update test for registerCandidate

--- src/test/kotlin/com/openlattice/chronicle/candidates/CandidatesApiTests.kt
+++ src/test/kotlin/com/openlattice/chronicle/candidates/CandidatesApiTests.kt
@@ -1,13 +1,20 @@
 package com.openlattice.chronicle.candidates
 
 import com.openlattice.chronicle.ChronicleServerTests
+import com.openlattice.chronicle.ids.IdConstants
+import org.junit.Assert
 import org.junit.Test
+import java.time.LocalDate
 
 class CandidatesApiTests : ChronicleServerTests() {
 
     @Test
     fun testRegisterCandidate() {
-        val c = Candidate(name = "ironman")
-        clientUser1.candidatesApi.registerCandidate(c)
+        val expected = Candidate(firstName = "iron", lastName = "man", dob = LocalDate.parse("2008-05-02"))
+        val candidateId = clientUser1.candidatesApi.registerCandidate(expected)
+        Assert.assertNotEquals(IdConstants.UNINITIALIZED.id, candidateId)
+        expected.candidateId = candidateId
+        val actual = clientUser1.candidatesApi.getCandidate(candidateId)
+        Assert.assertEquals(expected, actual)
     }
 }
