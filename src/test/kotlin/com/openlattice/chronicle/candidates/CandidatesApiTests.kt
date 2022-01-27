d4a666141f6830c60b20ce19876dcc8869656777
Hristo Oskov <hristo@openlattice.com>
2022-01-27 11:31:40 -0800

    fix tests

--- src/test/kotlin/com/openlattice/chronicle/candidates/CandidatesApiTests.kt
+++ src/test/kotlin/com/openlattice/chronicle/candidates/CandidatesApiTests.kt
@@ -2,35 +2,15 @@ package com.openlattice.chronicle.candidates
 
 import com.openlattice.chronicle.ChronicleServerTests
 import com.openlattice.chronicle.client.ChronicleClient
-import com.openlattice.chronicle.ids.IdConstants
 import org.junit.Test
-import java.lang.IllegalArgumentException
 
 class CandidatesApiTests : ChronicleServerTests() {
 
     private val chronicleClient: ChronicleClient = clientUser1
 
-    @Test(expected = IllegalArgumentException::class)
-    fun testBadCandidateDob1() {
-        chronicleClient.candidatesApi.registerCandidate(badCandidateDob("03-12-1989"))
-    }
-
-    @Test(expected = IllegalArgumentException::class)
-    fun testBadCandidateDob2() {
-        chronicleClient.candidatesApi.registerCandidate(badCandidateDob("03/12/1989"))
-    }
-
-    @Test(expected = IllegalArgumentException::class)
-    fun testBadCandidateDob3() {
-        chronicleClient.candidatesApi.registerCandidate(badCandidateDob("12-03-1989"))
-    }
-
-    @Test(expected = IllegalArgumentException::class)
-    fun testBadCandidateDob4() {
-        chronicleClient.candidatesApi.registerCandidate(badCandidateDob("12/03/1989"))
-    }
-
-    private fun badCandidateDob(dob: String): Candidate {
-        return Candidate(IdConstants.UNINITIALIZED.id, null, null, null, dob)
+    @Test
+    fun testRegisterCandidate() {
+        val c = Candidate(name = "ironman")
+        chronicleClient.candidatesApi.registerCandidate(c)
     }
 }
