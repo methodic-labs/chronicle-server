bba70948bcb19912cda031dd75a3c55d4ac655c8
Hristo Oskov <hristo@openlattice.com>
2022-01-27 11:32:26 -0800

    simplify

--- src/test/kotlin/com/openlattice/chronicle/candidates/CandidatesApiTests.kt
+++ src/test/kotlin/com/openlattice/chronicle/candidates/CandidatesApiTests.kt
@@ -1,16 +1,13 @@
 package com.openlattice.chronicle.candidates
 
 import com.openlattice.chronicle.ChronicleServerTests
-import com.openlattice.chronicle.client.ChronicleClient
 import org.junit.Test
 
 class CandidatesApiTests : ChronicleServerTests() {
 
-    private val chronicleClient: ChronicleClient = clientUser1
-
     @Test
     fun testRegisterCandidate() {
         val c = Candidate(name = "ironman")
-        chronicleClient.candidatesApi.registerCandidate(c)
+        clientUser1.candidatesApi.registerCandidate(c)
     }
 }
