c4f0e0ac24a2fc78f5f2e17be4618e7fcbec5cea
Hristo Oskov <hristo@openlattice.com>
2022-01-27 15:02:51 -0800

    testGetCandidates

--- src/test/kotlin/com/openlattice/chronicle/candidates/CandidatesApiTests.kt
+++ src/test/kotlin/com/openlattice/chronicle/candidates/CandidatesApiTests.kt
@@ -9,6 +9,19 @@ import java.time.LocalDate
 class CandidatesApiTests : ChronicleServerTests() {
 
     @Test
+    fun testGetCandidates() {
+        val c1 = Candidate(firstName = "iron", lastName = "man", dob = LocalDate.parse("2008-05-02"))
+        val c1Id = clientUser1.candidatesApi.registerCandidate(c1)
+        c1.candidateId = c1Id
+        val c2 = Candidate(firstName = "black", lastName = "panther", dob = LocalDate.parse("2018-02-16"))
+        val c2Id = clientUser1.candidatesApi.registerCandidate(c2)
+        c2.candidateId = c2Id
+        val expected = listOf(c1, c2)
+        val actual = clientUser1.candidatesApi.getCandidates(setOf(c1Id, c2Id))
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
     fun testRegisterCandidate() {
         val expected = Candidate(firstName = "iron", lastName = "man", dob = LocalDate.parse("2008-05-02"))
         val candidateId = clientUser1.candidatesApi.registerCandidate(expected)
