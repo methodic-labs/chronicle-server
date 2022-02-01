1f6c690af5b7dee85713d9c20fe0802c441d20eb
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-02-01 10:24:27 -1000

    Fix candidates api tests.

--- src/test/kotlin/com/openlattice/chronicle/candidates/CandidatesApiTests.kt
+++ src/test/kotlin/com/openlattice/chronicle/candidates/CandidatesApiTests.kt
@@ -10,12 +10,12 @@ class CandidatesApiTests : ChronicleServerTests() {
 
     @Test
     fun testGetCandidates() {
-        val c1 = Candidate(firstName = "iron", lastName = "man", dob = LocalDate.parse("2008-05-02"))
+        val c1 = Candidate(firstName = "iron", lastName = "man", dateOfBirth = LocalDate.parse("2008-05-02"))
         val c1Id = clientUser1.candidatesApi.registerCandidate(c1)
-        c1.candidateId = c1Id
-        val c2 = Candidate(firstName = "black", lastName = "panther", dob = LocalDate.parse("2018-02-16"))
+        c1.id = c1Id
+        val c2 = Candidate(firstName = "black", lastName = "panther", dateOfBirth = LocalDate.parse("2018-02-16"))
         val c2Id = clientUser1.candidatesApi.registerCandidate(c2)
-        c2.candidateId = c2Id
+        c2.id = c2Id
         val expected = listOf(c1, c2)
         val actual = clientUser1.candidatesApi.getCandidates(setOf(c1Id, c2Id))
         Assert.assertEquals(expected, actual)
@@ -23,10 +23,10 @@ class CandidatesApiTests : ChronicleServerTests() {
 
     @Test
     fun testRegisterCandidate() {
-        val expected = Candidate(firstName = "iron", lastName = "man", dob = LocalDate.parse("2008-05-02"))
+        val expected = Candidate(firstName = "iron", lastName = "man", dateOfBirth = LocalDate.parse("2008-05-02"))
         val candidateId = clientUser1.candidatesApi.registerCandidate(expected)
         Assert.assertNotEquals(IdConstants.UNINITIALIZED.id, candidateId)
-        expected.candidateId = candidateId
+        expected.id = candidateId
         val actual = clientUser1.candidatesApi.getCandidate(candidateId)
         Assert.assertEquals(expected, actual)
     }
--- src/test/kotlin/com/openlattice/chronicle/serialization/SourceDeviceSerializationTest.kt
+++ src/test/kotlin/com/openlattice/chronicle/serialization/SourceDeviceSerializationTest.kt
@@ -1,6 +1,10 @@
 package com.openlattice.chronicle.serialization
 
 import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.guava.GuavaModule
+import com.fasterxml.jackson.datatype.joda.JodaModule
 import com.geekbeast.serializer.serializer.AbstractJacksonSerializationTest
 import com.google.common.base.Optional
 import com.openlattice.chronicle.sources.AndroidDevice
@@ -17,7 +21,10 @@ class SourceDeviceSerializationTest : AbstractJacksonSerializationTest<SourceDev
     }
 
     override fun getSampleData(): SourceDevice {
-
+        val mapper2 = ObjectMapper()
+        mapper2.registerModule(GuavaModule())
+        mapper2.registerModule(JodaModule())
+        mapper2.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
         val o =  Optional.of(AndroidDevice(
             "Samsung",
             "P",
@@ -30,7 +37,7 @@ class SourceDeviceSerializationTest : AbstractJacksonSerializationTest<SourceDev
             Optional.of(mutableMapOf()))
         ) as Optional<SourceDevice>
 
-        val r =  mapper.writeValueAsString(o)
+        val r =  mapper2.writeValueAsString(o)
         logger.info( "JSON: $r")
         return  mapper.readValue(r, object: TypeReference<Optional<AndroidDevice>> () {} ).get()
     }
