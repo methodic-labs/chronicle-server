18be3bfc845445e20b3fdc1094c5e8194d49c1ae
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-02-01 10:56:13 -1000

    Clean up source device serialization test.

--- src/test/kotlin/com/openlattice/chronicle/serialization/SourceDeviceSerializationTest.kt
+++ src/test/kotlin/com/openlattice/chronicle/serialization/SourceDeviceSerializationTest.kt
@@ -14,35 +14,31 @@ import com.openlattice.chronicle.sources.SourceDevice
  *
  * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
  */
-class SourceDeviceSerializationTest : AbstractJacksonSerializationTest<SourceDevice>() {
+class SourceDeviceSerializationTest : AbstractJacksonSerializationTest<Optional<SourceDevice>>() {
 
-    override fun logResult(result: SerializationResult<SourceDevice>) {
+    override fun logResult(result: SerializationResult<Optional<SourceDevice>>) {
         logger.info("Actual JSON: ${result.jsonString}")
     }
 
-    override fun getSampleData(): SourceDevice {
-        val mapper2 = ObjectMapper()
-        mapper2.registerModule(GuavaModule())
-        mapper2.registerModule(JodaModule())
-        mapper2.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-        val o =  Optional.of(AndroidDevice(
-            "Samsung",
-            "P",
-            "Chocholate Chip",
-            "Samsung",
-            "21",
-            "21",
-            "",
-            "",
-            Optional.of(mutableMapOf()))
-        ) as Optional<SourceDevice>
+    override fun getSampleData(): Optional<SourceDevice> {
+        return Optional.of(
+            AndroidDevice(
+                "Samsung",
+                "P",
+                "Chocholate Chip",
+                "Samsung",
+                "21",
+                "21",
+                "",
+                "",
+                Optional.of(mutableMapOf())
+            )
+        )
 
-        val r =  mapper2.writeValueAsString(o)
-        logger.info( "JSON: $r")
-        return  mapper.readValue(r, object: TypeReference<Optional<AndroidDevice>> () {} ).get()
     }
 
-    override fun getClazz(): Class<SourceDevice> {
-        return SourceDevice::class.java
-    }
+    override fun getClazz(): Class<Optional<SourceDevice>>? = null
+    override fun getTypeReference(): TypeReference<Optional<SourceDevice>> =
+        object : TypeReference<Optional<SourceDevice>>() {}
+
 }
