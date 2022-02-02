9eee26010aa5b242e3a390e1650bd33fa84230a8
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-02-02 10:01:52 -0800

    add tests

--- src/test/kotlin/com/openlattice/chronicle/enrollment/EnrollmentTests.kt
+++ src/test/kotlin/com/openlattice/chronicle/enrollment/EnrollmentTests.kt
@@ -1,10 +1,12 @@
 package com.openlattice.chronicle.enrollment
 
+import com.auth0.json.mgmt.client.IOS
 import com.geekbeast.retrofit.RhizomeRetrofitCallFailedException
 import com.google.common.base.Optional
 import com.openlattice.chronicle.ChronicleServerTests
 import com.openlattice.chronicle.client.ChronicleClient
 import com.openlattice.chronicle.sources.AndroidDevice
+import com.openlattice.chronicle.sources.IOSDevice
 import com.openlattice.chronicle.study.Study
 import com.openlattice.chronicle.util.TestDataFactory
 import org.apache.commons.lang3.RandomStringUtils
@@ -44,6 +46,28 @@ class EnrollmentTests : ChronicleServerTests() {
         val candidateId = studyApi.registerParticipant(studyId, participant)
         LoggerFactory.getLogger(EnrollmentTests::class.java).info(" Serialization: ")
         val deviceId = studyApi.enroll(studyId, participantId, sourceDeviceId, sourceDevice)
+    }
+
+    @Test
+    fun testIOSEnrollment() {
+        val studyApi = chronicleClient.studyApi
+        val participant = TestDataFactory.participant()
+        val participantId = participant.participantId
+
+        val sourceDeviceId = "iosTestDevice"
+        val sourceDevice = IOSDevice(
+                "iPhone 13 Pro Max",
+                "iOS",
+                "iPhone",
+                "iPhone",
+                "15.2",
+                "61DB1549-A9E1-4C90-B567-D8657EDF19CB"
+        )
+        val study = Study(title = "Test Study", contact = "tests@openlattice.com")
+        val studyId = studyApi.createStudy(study)
+        studyApi.registerParticipant(studyId, participant)
+        val deviceId = studyApi.enroll(studyId, participantId, sourceDeviceId, sourceDevice)
+
     }
 
     @Test(expected=RhizomeRetrofitCallFailedException::class)
