ab4c3f871c8ad926d7b510ed66da65bb39001503
Solomon Tang <solomoncztang@gmail.com>
2022-02-03 13:41:34 -0800

    check permissions sql

--- src/main/kotlin/com/openlattice/chronicle/services/studies/StudyService.kt
+++ src/main/kotlin/com/openlattice/chronicle/services/studies/StudyService.kt
@@ -11,6 +11,7 @@ import com.openlattice.chronicle.auditing.AuditingComponent
 import com.openlattice.chronicle.auditing.AuditingManager
 import com.openlattice.chronicle.authorization.AclKey
 import com.openlattice.chronicle.authorization.AuthorizationManager
+import com.openlattice.chronicle.authorization.READ_PERMISSION
 import com.openlattice.chronicle.authorization.SecurableObjectType
 import com.openlattice.chronicle.authorization.principals.Principals
 import com.openlattice.chronicle.authorization.reservations.AclKeyReservationService
@@ -23,16 +24,20 @@ import com.openlattice.chronicle.services.candidates.CandidateManager
 import com.openlattice.chronicle.services.enrollment.EnrollmentManager
 import com.openlattice.chronicle.services.enrollment.EnrollmentService
 import com.openlattice.chronicle.storage.ChroniclePostgresTables.Companion.ORGANIZATION_STUDIES
+import com.openlattice.chronicle.storage.ChroniclePostgresTables.Companion.PERMISSIONS
 import com.openlattice.chronicle.study.Study
 import com.openlattice.chronicle.storage.ChroniclePostgresTables.Companion.STUDIES
 import com.openlattice.chronicle.storage.PostgresColumns
+import com.openlattice.chronicle.storage.PostgresColumns.Companion.ACL_KEY
 import com.openlattice.chronicle.storage.PostgresColumns.Companion.ORGANIZATION_ID
 import com.openlattice.chronicle.storage.PostgresColumns.Companion.ORGANIZATION_IDS
+import com.openlattice.chronicle.storage.PostgresColumns.Companion.PRINCIPAL_ID
 import com.openlattice.chronicle.storage.PostgresColumns.Companion.STUDY_ID
 import com.openlattice.chronicle.study.StudyUpdate
 import com.openlattice.chronicle.util.JsonFields.STUDY
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.security.Permissions
 import java.sql.Connection
 import java.time.OffsetDateTime
 import java.util.UUID
@@ -162,6 +167,10 @@ class StudyService(
         // This currently grabs all org studies regardless of access
         private val GET_ORG_STUDIES_SQL = """
             SELECT * from ${STUDIES.name}
+            INNER JOIN ${PERMISSIONS.name}
+            ON ${STUDIES.name}.${STUDY_ID.name} = ANY(${PERMISSIONS.name}.${ACL_KEY.name})
+                AND ${PRINCIPAL_ID.name} = ?
+                AND $READ_PERMISSION = ANY(${PostgresColumns.PERMISSIONS.name})
             WHERE ${STUDY_ID.name}
             IN (
                 SELECT ${STUDY_ID.name} FROM ${ORGANIZATION_STUDIES.name}
@@ -228,7 +237,8 @@ class StudyService(
     override fun getOrgStudies(organiaztionId: UUID): Iterable<Study> {
         return BasePostgresIterable(
             PreparedStatementHolderSupplier(storageResolver.getPlatformStorage(), GET_ORG_STUDIES_SQL, 256) { ps ->
-                ps.setObject(1, organiaztionId)
+                ps.setObject(1,Principals.getCurrentUser().id)
+                ps.setObject(2, organiaztionId)
                 ps.executeQuery()
             }
         ) { ResultSetAdapters.study(it) }
