457c4cfaaa079766c349c04d7909049565122d53
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-02-02 16:36:11 -0800

    write sensor data to redshift

--- src/main/kotlin/com/openlattice/chronicle/controllers/v3/IOSSensorController.kt
+++ src/main/kotlin/com/openlattice/chronicle/controllers/v3/IOSSensorController.kt
@@ -1,5 +1,6 @@
 package com.openlattice.chronicle.controllers.v3
 
+import com.openlattice.chronicle.data.ParticipationStatus
 import com.openlattice.chronicle.sensorkit.IOSSensorApi
 import com.openlattice.chronicle.sensorkit.IOSSensorApi.Companion.CONTROLLER
 import com.openlattice.chronicle.sensorkit.IOSSensorApi.Companion.DATASOURCE_ID
@@ -11,8 +12,11 @@ import com.openlattice.chronicle.sensorkit.IOSSensorApi.Companion.PARTICIPANT_ID
 import com.openlattice.chronicle.sensorkit.IOSSensorApi.Companion.STUDY_ID
 import com.openlattice.chronicle.sensorkit.IOSSensorApi.Companion.STUDY_ID_PATH
 import com.openlattice.chronicle.sensorkit.SensorDataSample
+import com.openlattice.chronicle.services.enrollment.EnrollmentManager
 import com.openlattice.chronicle.services.upload.IOSSensorDataManager
 import com.openlattice.chronicle.services.upload.IOSSensorDataService
+import com.openlattice.chronicle.util.ChronicleServerUtil
+import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -33,6 +37,13 @@ class IOSSensorController: IOSSensorApi {
     @Inject
     private lateinit var iosSensorDataService: IOSSensorDataService
 
+    @Inject
+    private lateinit var enrollmentManager: EnrollmentManager
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(IOSSensorController::class.java)
+    }
+
     @PostMapping(
             path = [ORGANIZATION_ID_PATH + STUDY_ID_PATH + PARTICIPANT_ID_PATH + DATASOURCE_ID_PATH],
             consumes = [MediaType.APPLICATION_JSON_VALUE]
@@ -43,6 +54,29 @@ class IOSSensorController: IOSSensorApi {
             @PathVariable (PARTICIPANT_ID) participantId: String,
             @PathVariable(DATASOURCE_ID)  deviceId: String,
             @RequestBody data: List<SensorDataSample>): Int {
+
+        val status = enrollmentManager.getParticipationStatus( studyId, participantId )
+        if (ParticipationStatus.NOT_ENROLLED == status) {
+            logger.warn(
+                    "participant is not enrolled, ignoring upload" + ChronicleServerUtil.ORG_STUDY_PARTICIPANT_DATASOURCE,
+                    organizationId,
+                    studyId,
+                    participantId,
+                    deviceId
+            )
+            return 0
+        }
+        val isDeviceEnrolled = enrollmentManager.isKnownDatasource( studyId, participantId, deviceId )
+        if (isDeviceEnrolled) {
+            logger.error(
+                    "data source not found, ignoring upload" + ChronicleServerUtil.ORG_STUDY_PARTICIPANT_DATASOURCE,
+                    organizationId,
+                    studyId,
+                    participantId,
+                    deviceId
+            )
+            return 0
+        }
         return iosSensorDataService.uploadData(organizationId, studyId, participantId, deviceId, data)
     }
 }
--- src/main/kotlin/com/openlattice/chronicle/services/upload/IOSSensorDataService.kt
+++ src/main/kotlin/com/openlattice/chronicle/services/upload/IOSSensorDataService.kt
@@ -1,11 +1,14 @@
 package com.openlattice.chronicle.services.upload
 
 import com.geekbeast.util.StopWatch
+import com.geekbeast.util.log
 import com.openlattice.chronicle.data.ParticipationStatus
 import com.openlattice.chronicle.sensorkit.SensorDataSample
 import com.openlattice.chronicle.services.enrollment.EnrollmentManager
+import com.openlattice.chronicle.storage.RedshiftDataTables.Companion.IOS_SENSOR
 import com.openlattice.chronicle.storage.StorageResolver
 import com.openlattice.chronicle.util.ChronicleServerUtil
+import com.zaxxer.hikari.HikariDataSource
 import org.slf4j.LoggerFactory
 import org.slf4j.event.Level
 import java.util.*
@@ -17,8 +20,30 @@ class IOSSensorDataService(
         private val storageResolver: StorageResolver,
         private val enrollmentManager: EnrollmentManager
 ) : IOSSensorDataManager {
-    private val logger = LoggerFactory.getLogger(IOSSensorDataService::class.java)
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(IOSSensorDataService::class.java)
+
+        private val COLUMNS = IOS_SENSOR.columns.joinToString(",") { it.name }
+
+        /**
+         * PreparedStatement bind order
+         * 1) organizationId,
+         * 2) studyId,
+         * 3) participantId,
+         * 4) sampleId
+         * 5) sensorName
+         * 6) dateRecorded
+         * 7) duration
+         * 8) timezone
+         * 9) sensorData,
+         * 10) device
+         */
+        private val INSERT_SENSOR_DATA_SQL = """
+            INSERT INTO ${IOS_SENSOR.name}($COLUMNS) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) 
+            ON CONFLICT DO NOTHING
+        """.trimIndent()
+    }
     override fun uploadData(
             organizationId: UUID,
             studyId: UUID,
@@ -40,30 +65,6 @@ class IOSSensorDataService(
             try {
                 val (flavor, hds) = storageResolver.resolveAndGetFlavor(studyId)
 
-
-                val status = enrollmentManager.getParticipationStatus( studyId, participantId )
-                if (ParticipationStatus.NOT_ENROLLED == status) {
-                    logger.warn(
-                            "participant is not enrolled, ignoring upload" + ChronicleServerUtil.ORG_STUDY_PARTICIPANT_DATASOURCE,
-                            organizationId,
-                            studyId,
-                            participantId,
-                            deviceId
-                    )
-                    return 0
-                }
-                val isDeviceEnrolled = enrollmentManager.isKnownDatasource( studyId, participantId, deviceId )
-                if (isDeviceEnrolled) {
-                    logger.error(
-                            "data source not found, ignoring upload" + ChronicleServerUtil.ORG_STUDY_PARTICIPANT_DATASOURCE,
-                            organizationId,
-                            studyId,
-                            participantId,
-                            deviceId
-                    )
-                    return 0
-                }
-
                 logger.info(
                         "attempting to log data" + ChronicleServerUtil.ORG_STUDY_PARTICIPANT_DATASOURCE,
                         organizationId,
@@ -71,7 +72,8 @@ class IOSSensorDataService(
                         participantId,
                         deviceId
                 )
-                val mappedData = mapToStorageModel(data)
+                val written = writeDataToStorage(hds, organizationId, studyId, participantId, deviceId, data)
+
                 return 0 // need to return number of rows written
             } catch (ex: Exception) {
                 return 0
@@ -79,7 +81,32 @@ class IOSSensorDataService(
         }
     }
 
-    private fun mapToStorageModel(data: List<SensorDataSample>) {
+    private fun writeDataToStorage(
+            hds: HikariDataSource,
+            organizationId: UUID,
+            studyId: UUID,
+            participantId: String,
+            deviceId: String,
+            data: List<SensorDataSample>
+    ) : Int {
 
+        return hds.connection.use { connection ->
+            try {
+                val wc = connection.prepareStatement(INSERT_SENSOR_DATA_SQL).use { ps ->
+                    ps.setString(1, organizationId.toString())
+//                    ps.setString(2, )
+                    ps.executeBatch().sum()
+                }
+                return@use wc
+            } catch (ex: Exception) {
+                logger.error(
+                        "error writing sensor data" + ChronicleServerUtil.ORG_STUDY_PARTICIPANT_DATASOURCE,
+                        organizationId,
+                        studyId,
+                        deviceId
+                )
+                return 0
+            }
+        }
     }
 }
