aaa0c4353f3b942c63c79727f8105ef7c3ab645e
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-02-04 14:07:01 -0800

    add columns for different sensor variables

--- src/main/kotlin/com/openlattice/chronicle/storage/RedshiftColumns.kt
+++ src/main/kotlin/com/openlattice/chronicle/storage/RedshiftColumns.kt
@@ -75,11 +75,68 @@ class RedshiftColumns {
         val DESCRIPTION = PostgresColumnDefinition("description", PostgresDatatype.TEXT_256).notNull()
         val DATA = PostgresColumnDefinition("data", PostgresDatatype.VARCHAR_MAX).notNull()
 
-        // sensorKit
-        val SENSOR_DATA = PostgresColumnDefinition("data", PostgresDatatype.JSONB).notNull()
+        // SENSOR_DATA
+        // these columns apply to all sensor types
+        val SAMPLE_ID = PostgresColumnDefinition("sample_id", PostgresDatatype.TEXT)
         val SENSOR_TYPE = PostgresColumnDefinition("sensor_type", PostgresDatatype.TEXT).notNull()
-        val SENSOR_DEVICE = PostgresColumnDefinition("device", PostgresDatatype.JSONB).notNull()
-        val SENSOR_DURATION = PostgresColumnDefinition("duration", PostgresDatatype.DOUBLE).notNull()
+        val SAMPLE_DURATION = PostgresColumnDefinition("sample_duration", PostgresDatatype.DOUBLE).notNull() // seconds
+        val DEVICE_VERSION = PostgresColumnDefinition("device_version", PostgresDatatype.TEXT).notNull()
+        val DEVICE_NAME = PostgresColumnDefinition("device_name", PostgresDatatype.TEXT).notNull()
+        val DEVICE_MODEL = PostgresColumnDefinition("device_model", PostgresDatatype.TEXT).notNull()
+        val DEVICE_SYSTEM_NAME = PostgresColumnDefinition("device_system_name", PostgresDatatype.TEXT)
+
+        // columns specific to deviceUsage sensor
+        val TOTAL_SCREEN_WAKES = PostgresColumnDefinition("total_screen_wakes", PostgresDatatype.INTEGER)
+        val TOTAL_UNLOCK_DURATION = PostgresColumnDefinition("total_unlock_duration", PostgresDatatype.DOUBLE) // seconds
+        val APP_CATEGORY = PostgresColumnDefinition("app_category", PostgresDatatype.TEXT)
+        val APP_CATEGORY_USAGE_TIME = PostgresColumnDefinition("usage_time", PostgresDatatype.DOUBLE)
+        val TEXT_INPUT_DEVICE = PostgresColumnDefinition("text_input_device", PostgresDatatype.TEXT)
+        val TEXT_INPUT_DURATION = PostgresColumnDefinition("text_input_duration", PostgresDatatype.TEXT)
+        val BUNDLE_IDENTIFIER = PostgresColumnDefinition("bundle_identifier", PostgresDatatype.TEXT)
+        val WEB_USAGE_DURATION = PostgresColumnDefinition("app_category_web_duration", PostgresDatatype.DOUBLE)
+        val NOTIFICATION_INTERACTION_TYPE = PostgresColumnDefinition("notification_interaction_type", PostgresDatatype.TEXT)
+
+        // columns specific to phoneUsage sensor
+        val TOTAL_INCOMING_CALLS = PostgresColumnDefinition("total_incoming_calls", PostgresDatatype.INTEGER)
+        val TOTAL_OUTGOING_CALLS = PostgresColumnDefinition("total_outgoing_calls", PostgresDatatype.INTEGER)
+        val TOTAL_CALL_DURATION = PostgresColumnDefinition("total_call_duration", PostgresDatatype.DOUBLE)
+        val TOTAL_UNIQUE_CONTACTS = PostgresColumnDefinition("total_unique_contacts", PostgresDatatype.INTEGER)
+
+        // messagesUsage sensor columns
+        val TOTAL_INCOMING_MESSAGES = PostgresColumnDefinition("total_incoming_messages", PostgresDatatype.INTEGER)
+        val TOTAL_OUTGOING_MESSAGES = PostgresColumnDefinition("total_outgoing_messages", PostgresDatatype.INTEGER)
+
+        // keyboard metrics sensor columns
+        val KEYBOARD_VERSION = PostgresColumnDefinition("keyboard_version", PostgresDatatype.TEXT)
+        val KEYBOARD_IDENTIFIER = PostgresColumnDefinition("keyboard_identifier", PostgresDatatype.TEXT)
+        val TOTAL_WORDS = PostgresColumnDefinition("total_words", PostgresDatatype.INTEGER)
+        val TOTAL_ALTERED_WORDS = PostgresColumnDefinition("total_altered_words", PostgresDatatype.INTEGER)
+        val TOTAL_TAPS = PostgresColumnDefinition("total_taps", PostgresDatatype.INTEGER)
+        val TOTAL_DRAGS = PostgresColumnDefinition("total_drags", PostgresDatatype.INTEGER)
+        val TOTAL_DELETES = PostgresColumnDefinition("total_deletes", PostgresDatatype.INTEGER)
+        val TOTAL_EMOJIS = PostgresColumnDefinition("total_emojis", PostgresDatatype.INTEGER)
+        val TOTAL_PATHS = PostgresColumnDefinition("total_paths", PostgresDatatype.INTEGER)
+        val TOTAL_PATH_TIME = PostgresColumnDefinition("total_path_time", PostgresDatatype.DOUBLE)
+        val TOTAL_PATH_LENGTH = PostgresColumnDefinition("total_path_length", PostgresDatatype.INTEGER)
+        val TOTAL_AUTO_CORRECTIONS = PostgresColumnDefinition("total_autocorrections", PostgresDatatype.INTEGER)
+        val TOTAL_SPACE_CORRECTIONS = PostgresColumnDefinition("total_space_corrections", PostgresDatatype.INTEGER)
+        val TOTAL_TRANSPOSITION_CORRECTIONS = PostgresColumnDefinition("total_transposition_corrections", PostgresDatatype.INTEGER)
+        val TOTAL_INSERT_KEY_CORRECTIONS = PostgresColumnDefinition("insert_key_corrections", PostgresDatatype.INTEGER)
+        val TOTAL_RETRO_CORRECTIONS = PostgresColumnDefinition("total_retro_corrections", PostgresDatatype.INTEGER)
+        val TOTAL_SKIP_TOUCH_CORRECTIONS = PostgresColumnDefinition("total_skip_touch_corrections", PostgresDatatype.INTEGER)
+        val TOTAL_NEAR_KEY_CORRECTIONS = PostgresColumnDefinition("total_near_key_corrections", PostgresDatatype.INTEGER)
+        val TOTAL_SUBSTITUTION_CORRECTIONS = PostgresColumnDefinition("total_substitution_corrections", PostgresDatatype.INTEGER)
+        val TOTAL_TEST_HIT_CORRECTIONS = PostgresColumnDefinition("total_test_hit_corrections", PostgresDatatype.INTEGER)
+        val TOTAL_TYPING_DURATION = PostgresColumnDefinition("total_typing_duration", PostgresDatatype.DOUBLE) // seconds
+        val TOTAL_PATH_PAUSES = PostgresColumnDefinition("total_path_pauses", PostgresDatatype.INTEGER)
+        val TOTAL_PAUSES = PostgresColumnDefinition("total_pauses", PostgresDatatype.INTEGER)
+        val TOTAL_TYPING_EPISODES = PostgresColumnDefinition("total_typing_episodes", PostgresDatatype.INTEGER)
+        val SENTIMENT = PostgresColumnDefinition("sentiment", PostgresDatatype.TEXT)
+        val SENTIMENT_WORD_COUNT = PostgresColumnDefinition("sentiment_word_count", PostgresDatatype.INTEGER)
+        val SENTIMENT_EMOJI_COUNT = PostgresColumnDefinition("sentiment_emoji_count", PostgresDatatype.INTEGER)
+        val TYPING_SPEED = PostgresColumnDefinition("typing_speed", PostgresDatatype.DOUBLE)
+        val PATH_TYPING_SPEED = PostgresColumnDefinition("path_typing_speed", PostgresDatatype.DOUBLE)
+
         val columnTypes : Map<String, PostgresDatatype> = redshiftColumns().associate { it.name to it.datatype }
 
         @JvmStatic
--- src/main/kotlin/com/openlattice/chronicle/storage/RedshiftDataTables.kt
+++ src/main/kotlin/com/openlattice/chronicle/storage/RedshiftDataTables.kt
@@ -4,30 +4,76 @@ import com.geekbeast.postgres.PostgresColumnDefinition
 import com.geekbeast.postgres.RedshiftTableDefinition
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.ACL_KEY
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.APPLICATION_LABEL
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.APP_CATEGORY
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.APP_CATEGORY_USAGE_TIME
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.APP_PACKAGE_NAME
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.AUDIT_EVENT_TYPE
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.BUNDLE_IDENTIFIER
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.DATA
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.DATE_LOGGED
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.DESCRIPTION
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.DEVICE_MODEL
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.DEVICE_NAME
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.DEVICE_SYSTEM_NAME
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.DEVICE_VERSION
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.DURATION
-import com.openlattice.chronicle.storage.RedshiftColumns.Companion.END_DATE_TIME
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.END_TIME
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.ID
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.INTERACTION_TYPE
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.KEYBOARD_IDENTIFIER
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.KEYBOARD_VERSION
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.NOTIFICATION_INTERACTION_TYPE
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.ORGANIZATION_ID
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.PARTICIPANT_ID
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.PATH_TYPING_SPEED
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.PRINCIPAL_ID
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.RECORDED_DATE_TIME
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.SAMPLE_DURATION
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.SAMPLE_ID
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.SECURABLE_PRINCIPAL_ID
-import com.openlattice.chronicle.storage.RedshiftColumns.Companion.SENSOR_DATA
-import com.openlattice.chronicle.storage.RedshiftColumns.Companion.SENSOR_DEVICE
-import com.openlattice.chronicle.storage.RedshiftColumns.Companion.SENSOR_DURATION
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.SENSOR_TYPE
-import com.openlattice.chronicle.storage.RedshiftColumns.Companion.START_DATE_TIME
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.SENTIMENT
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.SENTIMENT_EMOJI_COUNT
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.SENTIMENT_WORD_COUNT
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.START_TIME
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.STUDY_ID
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TEXT_INPUT_DEVICE
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TEXT_INPUT_DURATION
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TIMESTAMP
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TIMEZONE
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_ALTERED_WORDS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_AUTO_CORRECTIONS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_CALL_DURATION
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_DELETES
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_DRAGS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_EMOJIS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_INCOMING_CALLS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_INCOMING_MESSAGES
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_INSERT_KEY_CORRECTIONS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_NEAR_KEY_CORRECTIONS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_OUTGOING_CALLS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_OUTGOING_MESSAGES
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_PATHS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_PATH_LENGTH
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_PATH_PAUSES
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_PATH_TIME
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_PAUSES
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_RETRO_CORRECTIONS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_SCREEN_WAKES
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_SKIP_TOUCH_CORRECTIONS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_SPACE_CORRECTIONS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_SUBSTITUTION_CORRECTIONS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_TAPS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_TEST_HIT_CORRECTIONS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_TRANSPOSITION_CORRECTIONS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_TYPING_DURATION
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_TYPING_EPISODES
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_UNIQUE_CONTACTS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_UNLOCK_DURATION
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TOTAL_WORDS
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.TYPING_SPEED
 import com.openlattice.chronicle.storage.RedshiftColumns.Companion.USERNAME
+import com.openlattice.chronicle.storage.RedshiftColumns.Companion.WEB_USAGE_DURATION
 
 /**
  *
@@ -40,71 +86,136 @@ class RedshiftDataTables {
 
         @JvmField
         val CHRONICLE_USAGE_EVENTS = RedshiftTableDefinition("chronicle_usage_events")
-            .sortKey(STUDY_ID)
-            .addColumns(
-                ORGANIZATION_ID,
-                STUDY_ID,
-                PARTICIPANT_ID,
-                APP_PACKAGE_NAME,
-                INTERACTION_TYPE,
-                TIMESTAMP,
-                TIMEZONE,
-                USERNAME,
-                APPLICATION_LABEL
-            )
-            .addDataSourceNames(REDSHIFT_DATASOURCE_NAME)
-
-        @JvmField
-        val CHRONICLE_USAGE_STATS = RedshiftTableDefinition("chronicle_usage_stats")
-            .sortKey(STUDY_ID)
-            .addColumns(
-                ORGANIZATION_ID,
-                STUDY_ID,
-                PARTICIPANT_ID,
-                APP_PACKAGE_NAME,
-                INTERACTION_TYPE,
-                START_TIME,
-                END_TIME,
-                DURATION,
-                TIMESTAMP,
-                TIMEZONE,
-                APPLICATION_LABEL
-            )
-            .addDataSourceNames(REDSHIFT_DATASOURCE_NAME)
-
-
-        @JvmField
-        val AUDIT = RedshiftTableDefinition("audit")
-            .sortKey(ACL_KEY)
-            .addColumns(
-                ACL_KEY,
-                SECURABLE_PRINCIPAL_ID,
-                PRINCIPAL_ID,
-                AUDIT_EVENT_TYPE,
-                STUDY_ID,
-                ORGANIZATION_ID,
-                DESCRIPTION,
-                DATA,
-                TIMESTAMP
-            )
-            .addDataSourceNames(REDSHIFT_DATASOURCE_NAME)
-
-        @JvmField
-        val IOS_SENSOR = RedshiftTableDefinition("ios_sensor")
                 .sortKey(STUDY_ID)
                 .addColumns(
                         ORGANIZATION_ID,
                         STUDY_ID,
                         PARTICIPANT_ID,
-                        ID,
-                        SENSOR_TYPE,
-                        RECORDED_DATE_TIME,
-                        SENSOR_DURATION,
+                        APP_PACKAGE_NAME,
+                        INTERACTION_TYPE,
+                        TIMESTAMP,
                         TIMEZONE,
-                        SENSOR_DATA,
-                        SENSOR_DEVICE
+                        USERNAME,
+                        APPLICATION_LABEL
+                )
+                .addDataSourceNames(REDSHIFT_DATASOURCE_NAME)
+
+        @JvmField
+        val CHRONICLE_USAGE_STATS = RedshiftTableDefinition("chronicle_usage_stats")
+                .sortKey(STUDY_ID)
+                .addColumns(
+                        ORGANIZATION_ID,
+                        STUDY_ID,
+                        PARTICIPANT_ID,
+                        APP_PACKAGE_NAME,
+                        INTERACTION_TYPE,
+                        START_TIME,
+                        END_TIME,
+                        DURATION,
+                        TIMESTAMP,
+                        TIMEZONE,
+                        APPLICATION_LABEL
+                )
+                .addDataSourceNames(REDSHIFT_DATASOURCE_NAME)
+
+
+        @JvmField
+        val AUDIT = RedshiftTableDefinition("audit")
+                .sortKey(ACL_KEY)
+                .addColumns(
+                        ACL_KEY,
+                        SECURABLE_PRINCIPAL_ID,
+                        PRINCIPAL_ID,
+                        AUDIT_EVENT_TYPE,
+                        STUDY_ID,
+                        ORGANIZATION_ID,
+                        DESCRIPTION,
+                        DATA,
+                        TIMESTAMP
+                )
+                .addDataSourceNames(REDSHIFT_DATASOURCE_NAME)
+
+        // applies to all sensor types
+        val ALL_SENSOR_COLS = linkedSetOf(
+                STUDY_ID,
+                PARTICIPANT_ID,
+                ID,
+                SAMPLE_ID, // uniquely identifies a sample
+                SENSOR_TYPE,
+                RECORDED_DATE_TIME, //when sample was recorded by framework
+                SAMPLE_DURATION,
+                START_TIME, // lower date bound for sample record
+                END_TIME, // upper date bound for sample record
+                DEVICE_VERSION,
+                DEVICE_NAME,
+                DEVICE_MODEL,
+                DEVICE_SYSTEM_NAME
+        )
+
+        val DEVICE_USAGE_SENSOR_COLS = linkedSetOf(
+                TOTAL_SCREEN_WAKES,
+                TOTAL_UNLOCK_DURATION,
+                APP_CATEGORY,
+                APP_CATEGORY_USAGE_TIME,
+                TEXT_INPUT_DEVICE,
+                TEXT_INPUT_DURATION,
+                BUNDLE_IDENTIFIER,
+                WEB_USAGE_DURATION,
+                NOTIFICATION_INTERACTION_TYPE
+        )
+
+        val PHONE_USAGE_SENSOR_COLS = linkedSetOf(
+                TOTAL_INCOMING_CALLS,
+                TOTAL_OUTGOING_CALLS,
+                TOTAL_CALL_DURATION,
+                TOTAL_UNIQUE_CONTACTS
+        )
+
+        val MESSAGES_USAGE_SENSOR_COLS = linkedSetOf(
+                TOTAL_INCOMING_MESSAGES,
+                TOTAL_OUTGOING_MESSAGES,
+                TOTAL_UNIQUE_CONTACTS
+        )
+
+        val KEYBOARD_METRICS_SENSOR_COLS = linkedSetOf(
+                KEYBOARD_VERSION,
+                KEYBOARD_IDENTIFIER,
+                TOTAL_WORDS,
+                TOTAL_ALTERED_WORDS,
+                TOTAL_TAPS,
+                TOTAL_DRAGS,
+                TOTAL_DELETES,
+                TOTAL_EMOJIS,
+                TOTAL_PATHS,
+                TOTAL_PATH_TIME, //time to complete paths in seconds
+                TOTAL_PATH_LENGTH, //length of completed paths in cm
+                TOTAL_AUTO_CORRECTIONS,
+                TOTAL_SPACE_CORRECTIONS,
+                TOTAL_TRANSPOSITION_CORRECTIONS,
+                TOTAL_INSERT_KEY_CORRECTIONS,
+                TOTAL_RETRO_CORRECTIONS,
+                TOTAL_SKIP_TOUCH_CORRECTIONS,
+                TOTAL_NEAR_KEY_CORRECTIONS,
+                TOTAL_SUBSTITUTION_CORRECTIONS,
+                TOTAL_TEST_HIT_CORRECTIONS,
+                TOTAL_TYPING_DURATION, // seconds
+                TOTAL_PATH_PAUSES, //number of pauses while drawing path for a word
+                TOTAL_PAUSES,
+                TOTAL_TYPING_EPISODES, //number of continuous typing episodes
+                SENTIMENT,
+                SENTIMENT_WORD_COUNT,
+                SENTIMENT_EMOJI_COUNT,
+                TYPING_SPEED, // characters per second
+                PATH_TYPING_SPEED //QuickType words per minute
+        )
+        @JvmField
+        val IOS_SENSOR_DATA = RedshiftTableDefinition("sensor_data")
+                .sortKey(STUDY_ID)
+                .addColumns(
+                       *(ALL_SENSOR_COLS + DEVICE_USAGE_SENSOR_COLS + PHONE_USAGE_SENSOR_COLS + MESSAGES_USAGE_SENSOR_COLS +KEYBOARD_METRICS_SENSOR_COLS).toTypedArray()
                 ).primaryKey(ID)
                 .addDataSourceNames(REDSHIFT_DATASOURCE_NAME)
+
 
         private val USAGE_EVENT_COLS = CHRONICLE_USAGE_EVENTS.columns.joinToString(",") { it.name }
         private val USAGE_EVENT_PARAMS = CHRONICLE_USAGE_EVENTS.columns.joinToString(",") { "?" }
@@ -114,7 +225,7 @@ class RedshiftDataTables {
          */
         private fun getMergeClause(srcMergeTableName: String): String {
             return CHRONICLE_USAGE_EVENTS.columns.joinToString(
-                " AND "
+                    " AND "
             ) { "${CHRONICLE_USAGE_EVENTS.name}.${it.name} = ${srcMergeTableName}.${it.name}" }
         }
 
@@ -174,16 +285,16 @@ class RedshiftDataTables {
         """.trimIndent()
 
         val INSERT_USAGE_EVENT_COLUMN_INDICES: Map<String, Int> =
-            CHRONICLE_USAGE_EVENTS.columns.mapIndexed { index, pcd -> pcd.name to (index + 1) }.toMap() //remeber postgres is 1 based index
+                CHRONICLE_USAGE_EVENTS.columns.mapIndexed { index, pcd -> pcd.name to (index + 1) }.toMap() //remeber postgres is 1 based index
         val INSERT_USAGE_STATS_COLUMN_INDICES: Map<String, Int> =
-            CHRONICLE_USAGE_STATS.columns.mapIndexed { index, pcd -> pcd.name to (index + 1) }.toMap()
+                CHRONICLE_USAGE_STATS.columns.mapIndexed { index, pcd -> pcd.name to (index + 1) }.toMap()
 
         fun getInsertUsageEventColumnIndex(
-            column: PostgresColumnDefinition
+                column: PostgresColumnDefinition
         ): Int = INSERT_USAGE_EVENT_COLUMN_INDICES.getValue(column.name)
 
         fun getInsertUsageStatColumnIndex(
-            column: PostgresColumnDefinition
+                column: PostgresColumnDefinition
         ): Int = INSERT_USAGE_STATS_COLUMN_INDICES.getValue(column.name)
     }
 }
