88bd4828cd20265a88aef254362995b2faf4d4d4
Solomon Tang <solomoncztang@gmail.com>
2022-02-03 18:37:28 -0800

    update sql to use READ enum

--- src/main/kotlin/com/openlattice/chronicle/services/studies/StudyService.kt
+++ src/main/kotlin/com/openlattice/chronicle/services/studies/StudyService.kt
@@ -12,6 +12,7 @@ import com.openlattice.chronicle.auditing.AuditingManager
 import com.openlattice.chronicle.authorization.AclKey
 import com.openlattice.chronicle.authorization.Permission.READ
 import com.openlattice.chronicle.authorization.AuthorizationManager
+import com.openlattice.chronicle.authorization.Permission
 import com.openlattice.chronicle.authorization.SecurableObjectType
 import com.openlattice.chronicle.authorization.principals.Principals
 import com.openlattice.chronicle.hazelcast.HazelcastMap
@@ -200,7 +201,7 @@ class StudyService(
                 INNER JOIN ${PERMISSIONS.name}
                 ON ${STUDIES.name}.${STUDY_ID.name} = ANY(${PERMISSIONS.name}.${ACL_KEY.name})
                     AND ${PRINCIPAL_ID.name} = ?
-                    AND 'READ' = ANY(${PostgresColumns.PERMISSIONS.name})
+                    AND '${READ.name}' = ANY(${PostgresColumns.PERMISSIONS.name})
                 LEFT JOIN (
                     SELECT ${STUDY_ID.name}, array_agg(${ORGANIZATION_ID.name}) as ${ORGANIZATION_IDS.name} 
                         FROM ${ORGANIZATION_STUDIES.name}
