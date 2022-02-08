ee196a1d375b39680fcd0c62434187920d060604
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-02-08 15:48:53 -0800

    formatting

--- src/main/kotlin/com/openlattice/chronicle/controllers/v2/ChronicleControllerV2.kt
+++ src/main/kotlin/com/openlattice/chronicle/controllers/v2/ChronicleControllerV2.kt
@@ -18,12 +18,7 @@ import com.openlattice.chronicle.settings.AppComponent
 import com.openlattice.chronicle.sources.SourceDevice
 import org.apache.olingo.commons.api.edm.FullQualifiedName
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.security.InvalidParameterException
 import java.util.*
 import javax.inject.Inject
