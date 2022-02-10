2c3bfc606f9ff0954ed6a5364c5bc75dc84e5994
Solomon Tang <solomoncztang@gmail.com>
2022-02-09 18:19:03 -0800

    DeleteStudyJobData

---
+++ src/main/kotlin/com/openlattice/chronicle/jobs/DeleteStudyJobData.kt
@@ -0,0 +1,13 @@
+package com.openlattice.chronicle.jobs
+
+import com.openlattice.chronicle.storage.RedshiftDataTables.Companion.CHRONICLE_USAGE_EVENTS
+import java.util.*
+
+/**
+ * @author Solomon Tang <solomon@openlattice.com>
+ */
+class DeleteStudyUsageData(
+    val studyId: UUID
+): ChronicleJobData {
+    var table: String = CHRONICLE_USAGE_EVENTS.name
+}
