34ada9df2636344e9982645eabded8ed557ce89c
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-02-11 15:56:52 -0800

    correct endpoint path

--- src/main/kotlin/com/openlattice/chronicle/controllers/AppUsageSurveyController.kt
+++ src/main/kotlin/com/openlattice/chronicle/controllers/AppUsageSurveyController.kt
@@ -1,4 +1,4 @@
-package com.openlattice.chronicle.controllers.v3
+package com.openlattice.chronicle.controllers
 
 import com.codahale.metrics.annotation.Timed
 import com.openlattice.chronicle.services.surveys.SurveysService
@@ -6,8 +6,6 @@ import com.openlattice.chronicle.survey.AppUsage
 import com.openlattice.chronicle.survey.AppUsageSurveyApi
 import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.CONTROLLER
 import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.DATE
-import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.ORGANIZATION_ID
-import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.ORGANIZATION_ID_PATH
 import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.PARTICIPANT_ID
 import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.PARTICIPANT_ID_PATH
 import com.openlattice.chronicle.survey.AppUsageSurveyApi.Companion.STUDY_ID
@@ -36,29 +34,27 @@ class AppUsageSurveyController : AppUsageSurveyApi {
 
     @Timed
     @GetMapping(
-            path = [ORGANIZATION_ID_PATH + STUDY_ID_PATH + PARTICIPANT_ID_PATH],
+            path = [STUDY_ID_PATH + PARTICIPANT_ID_PATH],
             produces = [MediaType.APPLICATION_JSON_VALUE]
     )
     override fun getAppUsageData(
-            @PathVariable(ORGANIZATION_ID) organizationId: UUID,
             @PathVariable(STUDY_ID) studyId: UUID,
             @PathVariable(PARTICIPANT_ID) participantId: String,
             @RequestParam(value = DATE) date: String
     ): List<AppUsage> {
-        return surveysService.getAppUsageData(organizationId, studyId, participantId, date)
+        return surveysService.getAppUsageData(studyId, participantId, date)
     }
 
     @Timed
     @PostMapping(
-            path = [ORGANIZATION_ID_PATH + STUDY_ID_PATH + PARTICIPANT_ID_PATH],
+            path = [STUDY_ID_PATH + PARTICIPANT_ID_PATH],
             consumes = [MediaType.APPLICATION_JSON_VALUE],
     )
     override fun submitAppUsageSurvey(
-            @PathVariable(ORGANIZATION_ID) organizationId: UUID,
             @PathVariable(STUDY_ID) studyId: UUID,
             @PathVariable(PARTICIPANT_ID) participantId: String,
-            @RequestBody surveyResponses: Map<UUID, Set<String>>
+            @RequestBody surveyResponses: List<AppUsage>
     ) {
-        surveysService.submitAppUsageSurvey(organizationId, studyId, participantId, surveyResponses)
+        surveysService.submitAppUsageSurvey(studyId, participantId, surveyResponses)
     }
 }
