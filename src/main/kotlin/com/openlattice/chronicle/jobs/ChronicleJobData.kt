f6fa3613cbf632ee7e64de3fe69a5ba32e817a6b
Solomon Tang <solomoncztang@gmail.com>
2022-02-14 23:28:46 -0800

    add studyId to ChronicleJobData

--- src/main/kotlin/com/openlattice/chronicle/jobs/ChronicleJobData.kt
+++ src/main/kotlin/com/openlattice/chronicle/jobs/ChronicleJobData.kt
@@ -1,11 +1,12 @@
 package com.openlattice.chronicle.jobs
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import java.util.*
 
 /**
  * @author Solomon Tang <solomon@openlattice.com>
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@type")
 interface ChronicleJobData {
-
+    var studyId: UUID
 }
