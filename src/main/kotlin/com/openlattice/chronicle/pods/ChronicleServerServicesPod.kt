4ca53b61cc1a9d4311b9ebd6393dcdc520cb9cec
Hristo Oskov <hristo@openlattice.com>
2022-02-27 15:06:07 -0800

    missing param

--- src/main/kotlin/com/openlattice/chronicle/pods/ChronicleServerServicesPod.kt
+++ src/main/kotlin/com/openlattice/chronicle/pods/ChronicleServerServicesPod.kt
@@ -209,6 +209,7 @@ class ChronicleServerServicesPod {
             storageResolver,
             scheduledTasksManager(),
             enrollmentManager(),
+            studyService()
         )
     }
 
