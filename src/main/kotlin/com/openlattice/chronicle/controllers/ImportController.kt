c7ccd8a2cbc34757f76ed1bc012fd6f468d09235
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-02-28 10:57:40 -0800

    remove unused

--- src/main/kotlin/com/openlattice/chronicle/controllers/ImportController.kt
+++ src/main/kotlin/com/openlattice/chronicle/controllers/ImportController.kt
@@ -45,7 +45,6 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.sql.Array
-import java.sql.Date
 import java.sql.ResultSet
 import java.time.LocalDate
 import java.time.OffsetDateTime
