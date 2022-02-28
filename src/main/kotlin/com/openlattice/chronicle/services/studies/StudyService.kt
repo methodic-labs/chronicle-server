5aa7577d90d68fdc7388e41319dedff0d0ee95ac
Hristo Oskov <hristo@openlattice.com>
2022-02-28 01:21:53 -0800

    formatting

--- src/main/kotlin/com/openlattice/chronicle/services/studies/StudyService.kt
+++ src/main/kotlin/com/openlattice/chronicle/services/studies/StudyService.kt
@@ -558,7 +558,7 @@ class StudyService(
         }
     }
 
-    override fun isValidStudy(studyId: UUID ) : Boolean {
+    override fun isValidStudy(studyId: UUID): Boolean {
         val realStudyId = getStudyId(studyId)
         return realStudyId != null && studies.containsKey(realStudyId)
     }
