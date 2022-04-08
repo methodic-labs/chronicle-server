6f94ec304b3c770d397572c239c9448d581685ec
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-04-08 14:58:37 -0400

    Some additional logging.

--- src/main/kotlin/com/openlattice/chronicle/studies/tasks/StudyLimitsEnforcementTask.kt
+++ src/main/kotlin/com/openlattice/chronicle/studies/tasks/StudyLimitsEnforcementTask.kt
@@ -6,6 +6,7 @@ import com.openlattice.chronicle.data.ParticipationStatus
 import com.openlattice.chronicle.services.studies.StudyLimitsManager
 import com.openlattice.chronicle.services.studies.StudyManager
 import com.openlattice.chronicle.storage.StorageResolver
+import org.slf4j.LoggerFactory
 import java.util.concurrent.TimeUnit
 
 /**
@@ -13,6 +14,9 @@ import java.util.concurrent.TimeUnit
  * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
  */
 class StudyLimitsEnforcementTask : HazelcastFixedRateTask<StudyLimitsEnforcementTaskDependencies> {
+    companion object {
+        private val logger = LoggerFactory.getLogger(StudyLimitsEnforcementTask::class.java)
+    }
 
     override fun getInitialDelay(): Long = 10_000
 
@@ -33,6 +37,7 @@ class StudyLimitsEnforcementTask : HazelcastFixedRateTask<StudyLimitsEnforcement
     private fun pauseParticipantsForStudiesOverDuration() {
         val deps = getDependency()
         deps.studyLimitsManager.getStudiesExceedingDurationLimit().forEach { studyId ->
+            logger.info("Pausing data collection for all participants in study {}", studyId)
             deps.studyService.getStudyParticipants(studyId).forEach { participant ->
                 deps.studyService.updateParticipationStatus(
                     studyId,
@@ -46,9 +51,14 @@ class StudyLimitsEnforcementTask : HazelcastFixedRateTask<StudyLimitsEnforcement
     private fun deleteStudiesWhoseDataIsOutsideOfRetentionPeriod() {
         val deps = getDependency()
         deps.storageResolver.getPlatformStorage().connection.use { connection ->
+            val studiesToDelete = deps.studyLimitsManager.getStudiesExcceedingDataRetentionPeriod()
+            logger.info(
+                "Deleting the following studies as they are outside of the retention period: {}",
+                studiesToDelete
+            )
             deps.studyService.deleteStudies(
                 connection,
-                deps.studyLimitsManager.getStudiesExcceedingDataRetentionPeriod()
+                studiesToDelete
             )
         }
     }
