2cc37408f33833ec86821129150bcc34e2821cdf
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-04-09 15:07:10 -0400

    Remove unneeded import.

--- src/main/kotlin/com/openlattice/chronicle/pods/ChronicleConfigurationPod.kt
+++ src/main/kotlin/com/openlattice/chronicle/pods/ChronicleConfigurationPod.kt
@@ -6,9 +6,8 @@ import com.geekbeast.mail.MailServiceConfig
 import com.geekbeast.rhizome.pods.ConfigurationLoader
 import com.openlattice.chronicle.configuration.ChronicleConfiguration
 import com.openlattice.chronicle.configuration.TwilioConfiguration
-import com.openlattice.chronicle.upgrades.UpgradeService
 import com.openlattice.chronicle.storage.StorageResolver
-import com.openlattice.chronicle.upgrades.FixUpgrade
+import com.openlattice.chronicle.upgrades.UpgradeService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import javax.inject.Inject
@@ -49,5 +48,5 @@ class ChronicleConfigurationPod {
     fun upgradeService(): PreHazelcastUpgradeService {
         return UpgradeService(storageResolver())
     }
-    
+
 }
